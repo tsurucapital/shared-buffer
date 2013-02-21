bd4f0e1793799e7d594991c9e037243576530608
John Lato <jwlato@tsurucapital.com>
2013-02-21 14:03:28 +0800

    AtomicOps: add more docs

--- src/System/Posix/AtomicOps.hs
+++ src/System/Posix/AtomicOps.hs
@@ -1,11 +1,23 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-module System.Posix.AtomicOps where
+module System.Posix.AtomicOps (
+  -- * types
+  FPRef
+  -- * basic interface
+, newFPRef
+, readFPRef
+, writeFPRef
+
+  -- * FPRef Int operations
+, fetchAddFPRef
+, addFetchFPRef
+) where
 
 import Foreign.C.Types
 import Foreign.ForeignPtr
 import Foreign.Ptr
 import Foreign.Storable
 
+-- | A reference to a value held in a Foreign Ptr
 newtype FPRef a = FPRef { unFPRef :: ForeignPtr a }
 
 foreign import ccall unsafe "fetch_add_int64" c_fetch_add :: Ptr CInt -> CInt -> IO CInt
@@ -23,11 +35,11 @@ readFPRef (FPRef fp) = withForeignPtr fp peek
 writeFPRef :: Storable a => FPRef a -> a -> IO ()
 writeFPRef (FPRef fp) a = withForeignPtr fp (flip poke a)
 
--- fetch the previous value in an 'FPRef', and increment the FPRef by the given
+-- | fetch the previous value in an 'FPRef', and increment the FPRef by the given
 -- amount.  This operation is atomic.
 fetchAddFPRef :: FPRef Int -> Int -> IO Int
 fetchAddFPRef (FPRef fp) incr = withForeignPtr fp $ \p -> fromIntegral `fmap` c_fetch_add (castPtr p) (fromIntegral incr)
 
--- and increment the FPRef by the given amount and return the new value.  This operation is atomic.
+-- | increment the FPRef by the given amount and return the new value.  This operation is atomic.
 addFetchFPRef :: FPRef Int -> Int -> IO Int
 addFetchFPRef (FPRef fp) incr = withForeignPtr fp $ \p -> fromIntegral `fmap` c_add_fetch (castPtr p) (fromIntegral incr)
